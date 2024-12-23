b2b572b03348fe5d151f53f258f5b3f4e760d7f8
Manar <Abdumanar7@gmail.com>
2024-12-24 02:18:11 +0300

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,20 +1,5 @@
-package finalosprojectfrrrrrr;
-/*
+package RestaurantSimulation;
 
-Compiler:
-java version "1.8.0_371"
-NetBeans IDE 8.2
-
-Hardware Configuration:
-Processor	11th Gen Intel(R) Core(TM) i7-1165G7 @ 2.80GHz   2.80 GHz
-Installed RAM	16.0 GB (15.8 GB usable)
-
-Operating System: 
-Edition	Windows 11 Home
-Version	23H2
-
-
-*/
 import java.util.concurrent.Semaphore;
 
 public class Customer extends Thread {
