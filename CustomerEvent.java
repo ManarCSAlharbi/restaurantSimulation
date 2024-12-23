27e91d3845adf5ecb38cb1002cbce0ead3bcadcd
Manar <Abdumanar7@gmail.com>
2024-12-24 02:17:59 +0300

    Update CustomerEvent.java

--- CustomerEvent.java
+++ CustomerEvent.java
@@ -1,19 +1,5 @@
-package finalosprojectfrrrrrr;
-/*
-Compiler:
-java version "1.8.0_371"
-NetBeans IDE 8.2
+package RestaurantSimulation;
 
-Hardware Configuration:
-Processor	11th Gen Intel(R) Core(TM) i7-1165G7 @ 2.80GHz   2.80 GHz
-Installed RAM	16.0 GB (15.8 GB usable)
-
-Operating System: 
-Edition	Windows 11 Home
-Version	23H2
-
-
-*/
 public class CustomerEvent implements Comparable<CustomerEvent> {
     int customerId;
     String eventTime;
